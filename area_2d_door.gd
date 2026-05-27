9e9de513ab9ff2ba05468992c6e25f9ab62f1cc6
VLADIMIR GVORGYAN VEGA <105621819+Br3z4@users.noreply.github.com>
2026-05-27 20:48:49 +0800

    Add files via upload

---
+++ area_2d_door.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var entered = false
+
+
+
+func _on_body_entered(_body):
+	entered = true
+
+
+func _on_body_exited(_body):
+	entered = false
+
+
+func _process(_delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene_to_file("res://Tile Maps/2nd_Hallway.tscn")
+	
